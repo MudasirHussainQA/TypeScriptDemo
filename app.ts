256d19e30e716d83502bbfff4dc9e3302ef905d1
Mudasir Hussain <mudsrhussain@yahoo.com>
2020-08-15 08:30:29 +0530

    Added Enums

--- app.ts
+++ app.ts
@@ -1,33 +1,47 @@
-const person:{
-  name: string;
-  age:number;
-  hobbies: string[];
-  role: [number,string];
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
 
-}
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
 
- = {
-  name:'Mudasir',
-  age: 31,
-  hobbies: ['Cricket','Cycling'],
-  role: [1,'Automation Engineer']
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
 };
-person.role.push('admin');
-person.role.push('QA');
-person.role.push('BA');
-var value=person.role.lastIndexOf('admin');
-console.log("Value is",value);
 
-console.log("Designation:",person.role);
-let favoriteactivities: string[];
-favoriteactivities=['Cycling'];
-console.log(favoriteactivities);
-console.log(favoriteactivities[0]);
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 
 console.log(person.name);
 
-for(const hobby of person.hobbies)
-{
+for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
 
+if (person.role === Role.ADMIN) {
+  console.log('is author');
 }
---
+++ basic1.ts
@@ -0,0 +1,33 @@
+const person:{
+  name: string;
+  age:number;
+  hobbies: string[];
+  role: [number,string];
+
+}
+
+ = {
+  name:'Mudasir',
+  age: 31,
+  hobbies: ['Cricket','Cycling'],
+  role: [1,'Automation Engineer']
+};
+person.role.push('admin');
+person.role.push('QA');
+person.role.push('BA');
+var value=person.role.lastIndexOf('admin');
+console.log("Value is",value);
+
+console.log("Designation:",person.role);
+let favoriteactivities: string[];
+favoriteactivities=['Cycling'];
+console.log(favoriteactivities);
+console.log(favoriteactivities[0]);
+
+console.log(person.name);
+
+for(const hobby of person.hobbies)
+{
+  console.log(hobby.toUpperCase());
+
+}
